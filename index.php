7ac5f9d615dccface71068ff3a4603f16e9d5cc7
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2021-01-19 14:26:44 +0100

    feat: implement PostManager in index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 require 'vendor/autoload.php';
 
+use App\DatabaseManager;
+use App\PostManager;
 
+$pdo = DatabaseManager::getPdoInstance();
 
+PostManager::init();
+PostManager::importDefaultData();
+$posts = PostManager::getAllPosts();
+
+echo '<pre>';
+var_dump($posts);
+echo '</pre>';
+
+$post_0 = PostManager::getPostById("0");
+
+echo '<pre>';
+var_dump($post_0);
+echo '</pre>';
